8281d3c1362f93961bf3d39bf4893de4bc057bd8
Adam Rodger <adam.rodger@gmail.com>
2024-12-06 19:45:01 +0000

    day(6): Tidied up

--- src/AdventOfCode/Day6.cs
+++ src/AdventOfCode/Day6.cs
@@ -11,9 +11,45 @@ namespace AdventOfCode
     {
         public int Part1(string[] input)
         {
-            char[,] grid = input.ToGrid();
+            (char[,] grid, Point2D current) = Parse(input);
 
-            Point2D current = (-1, -1);
+            HashSet<Point2D> visited = Visit(grid, current);
+
+            return visited.Count;
+        }
+
+        public int Part2(string[] input)
+        {
+            (char[,] grid, Point2D start) = Parse(input);
+
+            IEnumerable<Point2D> candidates = Visit(grid, start);
+
+            int total = 0;
+
+            foreach (Point2D candidate in candidates)
+            {
+                grid[candidate.Y, candidate.X] = '#';
+
+                if (FormsLoop(grid, start))
+                {
+                    total++;
+                }
+
+                grid[candidate.Y, candidate.X] = '.';
+            }
+
+            return total;
+        }
+
+        /// <summary>
+        /// Parse the input
+        /// </summary>
+        /// <param name="input">Input</param>
+        /// <returns>Parsed grid and starting point</returns>
+        /// <exception cref="ArgumentException">Unable to find a starting point</exception>
+        private static (char[,] grid, Point2D start) Parse(string[] input)
+        {
+            char[,] grid = input.ToGrid();
 
             for (int y = 0; y < grid.GetLength(0); y++)
             {
@@ -21,13 +57,22 @@ namespace AdventOfCode
                 {
                     if (grid[y, x] == '^')
                     {
-                        current = (x, y);
+                        return (grid, (x, y));
                     }
                 }
             }
 
-            if (!current.InBounds(grid)) throw new IndexOutOfRangeException();
+            throw new ArgumentException("No start point found");
+        }
 
+        /// <summary>
+        /// Visit every point on the grid that can be reached from the given start point
+        /// </summary>
+        /// <param name="grid">Grid</param>
+        /// <param name="current">Start point</param>
+        /// <returns>All visited points on the grid</returns>
+        private static HashSet<Point2D> Visit(char[,] grid, Point2D current)
+        {
             Bearing bearing = Bearing.North;
 
             var visited = new HashSet<Point2D>();
@@ -35,9 +80,10 @@ namespace AdventOfCode
             while (current.InBounds(grid))
             {
                 visited.Add(current);
+
                 Point2D next = current.Move(bearing);
 
-                if (next.InBounds(grid) && grid[next.Y, next.X] == '#')
+                while (next.InBounds(grid) && grid[next.Y, next.X] == '#')
                 {
                     bearing = bearing.Turn(TurnDirection.Right);
                     next = current.Move(bearing);
@@ -46,46 +92,15 @@ namespace AdventOfCode
                 current = next;
             }
 
-            return visited.Count;
+            return visited;
         }
 
-        public int Part2(string[] input)
-        {
-            char[,] grid = input.ToGrid();
-
-            Point2D start = (-1, -1);
-
-            for (int y = 0; y < grid.GetLength(0); y++)
-            {
-                for (int x = 0; x < grid.GetLength(1); x++)
-                {
-                    if (grid[y, x] == '^')
-                    {
-                        start = (x, y);
-                    }
-                }
-            }
-
-            if (!start.InBounds(grid)) throw new IndexOutOfRangeException();
-
-            int total = 0;
-
-            for (int y = 0; y < grid.GetLength(0); y++)
-            {
-                for (int x = 0; x < grid.GetLength(1); x++)
-                {
-                    if (grid[y, x] != '.') continue;
-
-                    grid[y, x] = '#';
-                    if (FormsLoop(grid, start)) total++;
-                    grid[y, x] = '.';
-                }
-            }
-
-            // 1573 low
-            return total;
-        }
-
+        /// <summary>
+        /// Check if the given start point would form an infinite loop on the given grid
+        /// </summary>
+        /// <param name="grid">Grid</param>
+        /// <param name="start">Start point</param>
+        /// <returns>Whether a loop is formed or not</returns>
         private static bool FormsLoop(char[,] grid, Point2D start)
         {
             Point2D current = start;
@@ -97,10 +112,11 @@ namespace AdventOfCode
             {
                 if (!visited.Add((current, bearing)))
                 {
+                    // we've been here before, and we always will....
                     return true;
                 }
 
-                var next = current.Move(bearing);
+                Point2D next = current.Move(bearing);
 
                 while (next.InBounds(grid) && grid[next.Y, next.X] == '#')
                 {
