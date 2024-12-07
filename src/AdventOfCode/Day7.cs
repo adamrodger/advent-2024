d970e851da754c2d4c33ebb12d0c97da265aee06
Adam Rodger <adam.rodger@gmail.com>
2024-12-07 09:57:28 +0000

    day(7): Screwed up that exponentiation somehow

--- src/AdventOfCode/Day7.cs
+++ src/AdventOfCode/Day7.cs
@@ -1,6 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using AdventOfCode.Utilities;
 
 namespace AdventOfCode
@@ -32,9 +32,9 @@ namespace AdventOfCode
             return total;
         }
 
-        public long Part2(string[] input)
+        public BigInteger Part2(string[] input)
         {
-            long total = 0;
+            BigInteger total = 0;
 
             foreach (string line in input)
             {
@@ -51,6 +51,8 @@ namespace AdventOfCode
             }
 
             // 482739998262825 low
+
+            // 492383931650959
             return total;
         }
 
@@ -74,23 +76,24 @@ namespace AdventOfCode
 
         private static IEnumerable<long> PossibleSums2(long[] slice, long total)
         {
-            long magnitude = (long)Math.Pow(10, Math.Ceiling(Math.Log10(slice[0])));
-
-            if (slice.Length == 1)
+            if (slice.Length == 0)
             {
-                yield return total + slice[0];
-                yield return total * slice[0];
-                yield return (total * magnitude) + slice[0];
+                yield return total;
                 yield break;
             }
 
             IEnumerable<long> adds = PossibleSums2(slice[1..], total + slice[0]);
             IEnumerable<long> multiplies = PossibleSums2(slice[1..], total * slice[0]);
-            IEnumerable<long> concats = PossibleSums2(slice[1..], (total * magnitude) + slice[0]);
+            IEnumerable<long> concats = PossibleSums2(slice[1..], Concat(total, slice[0]));
 
             foreach (long next in adds.Concat(multiplies).Concat(concats))
             {
                 yield return next;
+            }
+
+            long Concat(long x, long y)
+            {
+                return long.Parse(x.ToString() + y.ToString());
             }
         }
     }
