4eabc090ef6ffddd41ff94828fba3b9b03a6c49e
Adam Rodger <adam.rodger@gmail.com>
2024-12-08 10:03:19 +0000

    day(8): Tidied up

--- src/AdventOfCode/Day8.cs
+++ src/AdventOfCode/Day8.cs
@@ -11,50 +11,58 @@ namespace AdventOfCode
     {
         public int Part1(string[] input)
         {
-            var antennae = new HashSet<(char Id, Point2D Point)>();
+            (char[,] grid, ILookup<char, Point2D> groups) = Parse(input);
+
             var antinodes = new HashSet<Point2D>();
 
-            char[,] grid = input.ToGrid();
-            grid.ForEach((x, y, c) =>
+            foreach ((Point2D top, Point2D bottom, Point2D vector) in Pairwise(groups))
             {
-                if (c != '.') antennae.Add((c, (x, y)));
-            });
+                Point2D higher = top - vector;
+                Point2D lower = bottom + vector;
 
-            ILookup<char, (char Id, Point2D Point)> groups = antennae.ToLookup(a => a.Id);
-
-            foreach (IGrouping<char, (char Id, Point2D Point)> group in groups)
-            {
-                foreach ((Point2D left, int i) in group.Select(((tuple, i) => (tuple.Point, i))))
+                if (higher.InBounds(grid))
                 {
-                    foreach (Point2D right in group.Skip(i + 1).Select(g => g.Point))
-                    {
-                        (Point2D top, Point2D bottom) = left.Y < right.Y
-                                                            ? (left, right)
-                                                            : (right, left);
+                    antinodes.Add(higher);
+                }
 
-                        Point2D vector = bottom - top;
-
-                        var higher = top - vector;
-                        var lower = bottom + vector;
-
-                        if (higher.InBounds(grid))
-                        {
-                            antinodes.Add(higher);
-                        }
-
-                        if (lower.InBounds(grid))
-                        {
-                            antinodes.Add(lower);
-                        }
-                    }
+                if (lower.InBounds(grid))
+                {
+                    antinodes.Add(lower);
                 }
             }
 
-            // 2499 high
             return antinodes.Count;
         }
 
         public int Part2(string[] input)
+        {
+            (char[,] grid, ILookup<char, Point2D> groups) = Parse(input);
+
+            var antinodes = new HashSet<Point2D>();
+
+            foreach ((Point2D top, Point2D bottom, Point2D vector) in Pairwise(groups))
+            {
+                Point2D higher = top + vector; // first iteration will just hit the other node position
+
+                while (higher.InBounds(grid))
+                {
+                    antinodes.Add(higher);
+                    higher += vector;
+                }
+
+                Point2D lower = bottom - vector;
+
+                while (lower.InBounds(grid))
+                {
+                    antinodes.Add(lower);
+                    lower -= vector;
+                }
+            }
+
+            return antinodes.Count;
+        }
+
+        private static (char[,] Grid, ILookup<char, Point2D> Antennae) Parse(string[] input)
         {
             var antennae = new HashSet<(char Id, Point2D Point)>();
 
@@ -64,44 +72,24 @@ namespace AdventOfCode
                 if (c != '.') antennae.Add((c, (x, y)));
             });
 
-            var antinodes = antennae.Select(pair => pair.Point).ToHashSet();
+            // groups will always be in ascending order of Y
+            ILookup<char, Point2D> groups = antennae.ToLookup(a => a.Id, a => a.Point);
 
-            ILookup<char, (char Id, Point2D Point)> groups = antennae.ToLookup(a => a.Id);
+            return (grid, groups);
+        }
 
-            foreach (IGrouping<char, (char Id, Point2D Point)> group in groups)
+        private static IEnumerable<(Point2D Top, Point2D Bottom, Point2D Vector)> Pairwise(ILookup<char, Point2D> antennae)
+        {
+            foreach (IGrouping<char, Point2D> group in antennae)
             {
-                foreach ((Point2D left, int i) in group.Select(((tuple, i) => (tuple.Point, i))))
+                foreach ((Point2D top, int i) in group.Select(((point, i) => (point, i))))
                 {
-                    foreach (Point2D right in group.Skip(i + 1).Select(g => g.Point))
+                    foreach (Point2D bottom in group.Skip(i + 1))
                     {
-                        (Point2D top, Point2D bottom) = left.Y < right.Y
-                                                            ? (left, right)
-                                                            : (right, left);
-
-                        Point2D vector = bottom - top;
-
-
-
-                        var higher = top - vector;
-
-                        while (higher.InBounds(grid))
-                        {
-                            antinodes.Add(higher);
-                            higher = higher - vector;
-                        }
-
-                        var lower = bottom + vector;
-
-                        while (lower.InBounds(grid))
-                        {
-                            antinodes.Add(lower);
-                            lower = lower + vector;
-                        }
+                        yield return (top, bottom, bottom - top);
                     }
                 }
             }
-
-            return antinodes.Count;
         }
     }
 }
