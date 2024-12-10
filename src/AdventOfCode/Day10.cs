6f02d560dc3968bd444927a0c021df208401d90d
Adam Rodger <adam.rodger@gmail.com>
2024-12-10 16:17:06 +0000

    day(10): Tidied up

--- src/AdventOfCode/Day10.cs
+++ src/AdventOfCode/Day10.cs
@@ -11,43 +11,31 @@ namespace AdventOfCode
     {
         public int Part1(string[] input)
         {
-            var grid = input.ToGrid<int>();
-            int total = 0;
+            var grid = input.ToGrid();
 
-            for (int y = 0; y < grid.GetLength(0); y++)
-            {
-                for (int x = 0; x < grid.GetLength(1); x++)
-                {
-                    if (grid[y, x] == 0)
-                    {
-                        total += PossiblePeaks(grid, (x, y));
-                    }
-                }
-            }
-
-            return total;
+            return grid.Points()
+                       .Where(p => grid.At(p) == '0')
+                       .Select(p => PossiblePeaks(grid, p))
+                       .Sum();
         }
 
         public int Part2(string[] input)
         {
-            var grid = input.ToGrid<int>();
-            int total = 0;
+            var grid = input.ToGrid();
 
-            for (int y = 0; y < grid.GetLength(0); y++)
-            {
-                for (int x = 0; x < grid.GetLength(1); x++)
-                {
-                    if (grid[y, x] == 0)
-                    {
-                        total += PossiblePeaks2(grid, (x, y));
-                    }
-                }
-            }
-
-            return total;
+            return grid.Points()
+                       .Where(p => grid.At(p) == '0')
+                       .Select(p => PossiblePaths(grid, p))
+                       .Sum();
         }
 
-        private static int PossiblePeaks(int[,] grid, Point2D start)
+        /// <summary>
+        /// Count the possible number of reachable peaks from the start point
+        /// </summary>
+        /// <param name="grid">Grid</param>
+        /// <param name="start">Start point</param>
+        /// <returns>Possible reachable peaks</returns>
+        private static int PossiblePeaks(char[,] grid, Point2D start)
         {
             int score = 0;
 
@@ -63,15 +51,15 @@ namespace AdventOfCode
                     continue;
                 }
 
-                int height = grid[current.Y, current.X];
+                char height = grid[current.Y, current.X];
 
-                if (height == 9)
+                if (height == '9')
                 {
                     score++;
                     continue;
                 }
 
-                foreach (Point2D next in grid.Adjacent4Positions(current).Where(n => grid[n.Y, n.X] - height == 1 && !visited.Contains(n)))
+                foreach (Point2D next in grid.Adjacent4Positions(current).Where(n => grid.At(n) - height == 1))
                 {
                     queue.Enqueue(next);
                 }
@@ -80,9 +68,13 @@ namespace AdventOfCode
             return score;
         }
 
-
-
-        private static int PossiblePeaks2(int[,] grid, Point2D start)
+        /// <summary>
+        /// Count the possible paths from the start point that can reach a peak
+        /// </summary>
+        /// <param name="grid">Grid</param>
+        /// <param name="start">Start point</param>
+        /// <returns>Possible paths</returns>
+        private static int PossiblePaths(char[,] grid, Point2D start)
         {
             int score = 0;
 
@@ -92,15 +84,15 @@ namespace AdventOfCode
             {
                 Point2D current = queue.Dequeue();
 
-                int height = grid[current.Y, current.X];
+                char height = grid[current.Y, current.X];
 
-                if (height == 9)
+                if (height == '9')
                 {
                     score++;
                     continue;
                 }
 
-                foreach (Point2D next in grid.Adjacent4Positions(current).Where(n => grid[n.Y, n.X] - height == 1))
+                foreach (Point2D next in grid.Adjacent4Positions(current).Where(n => grid.At(n) - height == 1))
                 {
                     queue.Enqueue(next);
                 }
