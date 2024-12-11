07b5b05afd14e0307c3714de512ab8a9de808a4b
Adam Rodger <adam.rodger@gmail.com>
2024-12-11 09:23:48 +0000

    day(11): Part 2

--- src/AdventOfCode/Day11.cs
+++ src/AdventOfCode/Day11.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
+using AdventOfCode.Utilities;
 
 namespace AdventOfCode
 {
@@ -16,7 +16,6 @@ namespace AdventOfCode
 
             for (int i = 0; i < 25; i++)
             {
-                Debug.WriteLine(stones.Length);
                 stones = Blink(stones).ToArray();
             }
 
@@ -24,14 +23,76 @@ namespace AdventOfCode
             return stones.Length;
         }
 
-        public int Part2(string[] input)
+        public long Part2(string[] input)
         {
-            foreach (string line in input)
-            {
-                throw new NotImplementedException("Part 2 not implemented");
-            }
+            long[] stones = input[0].Split(' ').Select(long.Parse).ToArray();
+            Dictionary<long, long> counts = stones.ToDictionary(s => s, _ => 1L);
 
-            return 0;
+            for (int i = 0; i < 75; i++)
+            {
+                var temp = new Dictionary<long, long>();
+
+                foreach ((long stone, long count) in counts)
+                {
+                    if (stone == 0)
+                    {
+                        temp[1] = temp.GetOrDefault(1) + count;
+                        continue;
+                    }
+
+                    int digits = (int)Math.Log10(stone) + 1;
+
+                    if (digits % 2 == 0)
+                    {
+                        int power = (int)Math.Pow(10, (int)(digits / 2));
+                        (long left, long right) = Math.DivRem(stone, power);
+                        temp[left] = temp.GetOrDefault(left) + count;
+                        temp[right] = temp.GetOrDefault(right) + count;
+                    }
+                    else
+                    {
+                        temp[stone * 2024] = temp.GetOrDefault(stone * 2024) + count;
+                    }
+                }
+
+                counts = temp;
+            }
+            
+            return counts.Values.Sum();
+
+
+            //List<int> counts = [stones.Length];
+            //List<int> hashes = [Hash(stones)];
+
+            //for (int i = 0; i < 35; i++)
+            //{
+            //    stones = Blink(stones).ToArray();
+            //    counts.Add(stones.Length);
+
+            //    hashes.Add(Hash(stones));
+            //}
+
+            //foreach ((int First, int Second) in counts.Zip(hashes))
+            //{
+            //    Debug.WriteLine($"{First,15}{(Second),25}");
+            //}
+
+            //bool repeats = hashes.ToHashSet().Count != hashes.Count;
+            //Debug.WriteLine($"Repeats? {repeats}");
+
+            //return stones.Length;
+
+            //static int Hash(IEnumerable<long> n)
+            //{
+            //    var hasher = new HashCode();
+
+            //    foreach (long x in n.Take(100))
+            //    {
+            //        hasher.Add(x);
+            //    }
+
+            //    return hasher.ToHashCode();
+            //}
         }
 
         private static IEnumerable<long> Blink(IEnumerable<long> stones)
